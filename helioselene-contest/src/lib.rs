2f3da7668ffc16fb05e5bb9d9991bd7bd24495b6
j-berman <justinberman@protonmail.com>
2025-02-12 11:49:43 -0800

    Helios/Selene point wasm tests

--- helioselene-contest/src/lib.rs
+++ helioselene-contest/src/lib.rs
@@ -63,18 +63,61 @@ pub fn custom_getrandom(buf: &mut [u8]) -> Result<(), Error> {
 register_custom_getrandom!(custom_getrandom);
 
 // Tests for https://github.com/kayabaNerve/wasm-cycles
-fn init_ref_scalars() -> (HelioseleneFieldRef, HelioseleneFieldRef) {
+fn init_ref_helios_scalars() -> (HelioseleneFieldRef, HelioseleneFieldRef) {
     let mut rng = unsafe { ChaCha20Rng::from_seed(RNG_SEED) };
     let a = HelioseleneFieldRef::random(&mut rng);
     let b = HelioseleneFieldRef::random(&mut rng);
     (a, b)
 }
 
-fn init_contest_scalars() -> (HelioseleneField, HelioseleneField) {
+fn init_contest_helios_scalars() -> (HelioseleneField, HelioseleneField) {
     let mut rng = unsafe { ChaCha20Rng::from_seed(RNG_SEED) };
     let a = HelioseleneField::random(&mut rng);
     let b = HelioseleneField::random(&mut rng);
     (a, b)
+}
+
+fn init_ref_selene_scalars() -> (Field25519Ref, Field25519Ref) {
+    let mut rng = unsafe { ChaCha20Rng::from_seed(RNG_SEED) };
+    let a = Field25519Ref::random(&mut rng);
+    let b = Field25519Ref::random(&mut rng);
+    (a, b)
+}
+
+fn init_contest_selene_scalars() -> (Field25519, Field25519) {
+    let mut rng = unsafe { ChaCha20Rng::from_seed(RNG_SEED) };
+    let a = Field25519::random(&mut rng);
+    let b = Field25519::random(&mut rng);
+    (a, b)
+}
+
+// Get random scalar first and mul by G to guarantee constant time
+fn init_ref_helios_points() -> (HeliosPointRef, HeliosPointRef) {
+    let (a, b) = init_ref_helios_scalars();
+    let A = HeliosPointRef::generator() * a;
+    let B = HeliosPointRef::generator() * b;
+    (A, B)
+}
+
+fn init_contest_helios_points() -> (HeliosPoint, HeliosPoint) {
+    let (a, b) = init_contest_helios_scalars();
+    let A = HeliosPoint::generator() * a;
+    let B = HeliosPoint::generator() * b;
+    (A, B)
+}
+
+fn init_ref_selene_points() -> (SelenePointRef, SelenePointRef) {
+    let (a, b) = init_ref_selene_scalars();
+    let A = SelenePointRef::generator() * a;
+    let B = SelenePointRef::generator() * b;
+    (A, B)
+}
+
+fn init_contest_selene_points() -> (SelenePoint, SelenePoint) {
+    let (a, b) = init_contest_selene_scalars();
+    let A = SelenePoint::generator() * a;
+    let B = SelenePoint::generator() * b;
+    (A, B)
 }
 
 macro_rules! define_case {
@@ -109,13 +152,13 @@ macro_rules! define_test_a_op_b {
 
             #[no_mangle]
             pub extern "C" fn [<test_ $fn_name _ref>]() {
-                let (a, b) = core::hint::black_box(init_ref_scalars());
+                let (a, b) = core::hint::black_box(init_ref_helios_scalars());
                 let _ = core::hint::black_box(a $op b);
             }
 
             #[no_mangle]
             pub extern "C" fn [<test_ $fn_name _contest>]() {
-                let (a, b) = core::hint::black_box(init_contest_scalars());
+                let (a, b) = core::hint::black_box(init_contest_helios_scalars());
                 let _ = core::hint::black_box(a $op b);
             }
         }
@@ -129,13 +172,13 @@ macro_rules! define_test_a_dot_method {
 
             #[no_mangle]
             pub extern "C" fn [<test_ $fn_name _ref>]() {
-                let (a, _) = core::hint::black_box(init_ref_scalars());
+                let (a, _) = core::hint::black_box(init_ref_helios_scalars());
                 let _ = core::hint::black_box(a.$method());
             }
 
             #[no_mangle]
             pub extern "C" fn [<test_ $fn_name _contest>]() {
-                let (a, _) = core::hint::black_box(init_contest_scalars());
+                let (a, _) = core::hint::black_box(init_contest_helios_scalars());
                 let _ = core::hint::black_box(a.$method());
             }
         }
@@ -144,40 +187,166 @@ macro_rules! define_test_a_dot_method {
 
 #[no_mangle]
 pub extern "C" fn test_helioselene_sqrt_ref() {
-    let (a, _) = core::hint::black_box(init_ref_scalars());
+    let (a, _) = core::hint::black_box(init_ref_helios_scalars());
     let _ = core::hint::black_box(a.square().sqrt().unwrap());
 }
 
 #[no_mangle]
 pub extern "C" fn test_helioselene_sqrt_contest() {
-    let (a, _) = core::hint::black_box(init_contest_scalars());
+    let (a, _) = core::hint::black_box(init_contest_helios_scalars());
     let _ = core::hint::black_box(a.square().sqrt().unwrap());
 }
 
 #[no_mangle]
 pub extern "C" fn test_helioselene_pow_ref() {
-    let (a, b) = core::hint::black_box(init_ref_scalars());
+    let (a, b) = core::hint::black_box(init_ref_helios_scalars());
     let _ = core::hint::black_box(a.pow(b));
 }
 
 #[no_mangle]
 pub extern "C" fn test_helioselene_pow_contest() {
-    let (a, b) = core::hint::black_box(init_contest_scalars());
+    let (a, b) = core::hint::black_box(init_contest_helios_scalars());
     let _ = core::hint::black_box(a.pow(b));
 }
 
 #[no_mangle]
 pub extern "C" fn test_helioselene_neg_ref() {
-    let (a, _) = core::hint::black_box(init_ref_scalars());
+    let (a, _) = core::hint::black_box(init_ref_helios_scalars());
     let _ = core::hint::black_box(-a);
 }
 
 #[no_mangle]
 pub extern "C" fn test_helioselene_neg_contest() {
-    let (a, _) = core::hint::black_box(init_contest_scalars());
+    let (a, _) = core::hint::black_box(init_contest_helios_scalars());
     let _ = core::hint::black_box(-a);
 }
 
+macro_rules! define_point_tests {
+    ($point_name:ident, $typename:ident) => {
+        paste! {
+            define_cases!([<$point_name _init_points>]);
+            define_cases!([<$point_name _init_scalars>]);
+            define_cases!([<$point_name _add>]);
+            define_cases!([<$point_name _mul>]);
+            define_cases!([<$point_name _mul_by_generator>]);
+            define_cases!([<$point_name _sub>]);
+            define_cases!([<$point_name _dbl>]);
+            define_cases!([<$point_name _neg>]);
+            define_cases!([<$point_name _compression>]);
+            define_cases!([<$point_name _decompression>]);
+
+            // init_points and init_scalars are useful to noramlize final
+            // results (subtract respective cycles to init from cycle count)
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _init_points_ref>]() {
+                let (_, _) = core::hint::black_box([<init_ref_ $point_name _points>]());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _init_points_contest>]() {
+                let (_, _) = core::hint::black_box([<init_contest_ $point_name _points>]());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _init_scalars_ref>]() {
+                let (_, _) = core::hint::black_box([<init_ref_ $point_name _scalars>]());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _init_scalars_contest>]() {
+                let (_, _) = core::hint::black_box([<init_contest_ $point_name _scalars>]());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _add_ref>]() {
+                let (A, B) = core::hint::black_box([<init_ref_ $point_name _points>]());
+                let _ = core::hint::black_box(A + B);
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _add_contest>]() {
+                let (A, B) = core::hint::black_box([<init_contest_ $point_name _points>]());
+                let _ = core::hint::black_box(A + B);
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _mul_ref>]() {
+                let (A, _) = core::hint::black_box([<init_ref_ $point_name _points>]());
+                let (s, _) = core::hint::black_box([<init_ref_ $point_name _scalars>]());
+                let _ = core::hint::black_box(A * s);
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _mul_contest>]() {
+                let (A, _) = core::hint::black_box([<init_contest_ $point_name _points>]());
+                let (s, _) = core::hint::black_box([<init_contest_ $point_name _scalars>]());
+                let _ = core::hint::black_box(A * s);
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _mul_by_generator_ref>]() {
+                let (a, _) = core::hint::black_box([<init_ref_ $point_name _scalars>]());
+                let _ = core::hint::black_box([<$typename PointRef>]::generator() * a);
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _mul_by_generator_contest>]() {
+                let (a, _) = core::hint::black_box([<init_contest_ $point_name _scalars>]());
+                let _ = core::hint::black_box([<$typename Point>]::generator() * a);
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _sub_ref>]() {
+                let (A, B) = core::hint::black_box([<init_ref_ $point_name _points>]());
+                let _ = core::hint::black_box(A - B);
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _sub_contest>]() {
+                let (A, B) = core::hint::black_box([<init_contest_ $point_name _points>]());
+                let _ = core::hint::black_box(A - B);
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _dbl_ref>]() {
+                let (A, _) = core::hint::black_box([<init_ref_ $point_name _points>]());
+                let _ = core::hint::black_box(A.double());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _dbl_contest>]() {
+                let (A, _) = core::hint::black_box([<init_contest_ $point_name _points>]());
+                let _ = core::hint::black_box(A.double());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _compression_ref>]() {
+                let (A, _) = core::hint::black_box([<init_ref_ $point_name _points>]());
+                let _ = core::hint::black_box(A.to_bytes());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _compression_contest>]() {
+                let (A, _) = core::hint::black_box([<init_contest_ $point_name _points>]());
+                let _ = core::hint::black_box(A.to_bytes());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _decompression_ref>]() {
+                let (A, _) = core::hint::black_box([<init_ref_ $point_name _points>]());
+                let _ = core::hint::black_box([<$typename PointRef>]::from_bytes(&A.to_bytes()).unwrap());
+            }
+
+            #[no_mangle]
+            pub extern "C" fn [<test_ $point_name _decompression_contest>]() {
+                let (A, _) = core::hint::black_box([<init_contest_ $point_name _points>]());
+                let _ = core::hint::black_box([<$typename Point>]::from_bytes(&A.to_bytes()).unwrap());
+            }
+        }
+    };
+}
+
+// Field wasm tests
 define_test_a_op_b!(helioselene_add, +);
 define_test_a_op_b!(helioselene_mul, *);
 define_test_a_op_b!(helioselene_sub, -);
@@ -189,3 +358,7 @@ define_test_a_dot_method!(helioselene_evn, is_even);
 define_cases!(helioselene_sqrt);
 define_cases!(helioselene_pow);
 define_cases!(helioselene_neg);
+
+// Point wasm tests
+define_point_tests!(helios, Helios);
+define_point_tests!(selene, Selene);
